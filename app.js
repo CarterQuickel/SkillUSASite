c8dd4c4afcb641fe95603d32a6434168840e929c
gabby11s <27GStevens@live.ytech.edu>
2026-05-05 11:04:07 -0400

    Progress 5/5/26
    Stopped because I was rate limited. I created clickable pages for everything, which includes Home, About, Staff, Contact, Competitions, Donate, Sponsor, News, and Photos. However, Contact is incomplete and there are no forms for Contact, Donate, or Sponsor. Pages needing header images were properly implemented. I was rate limited, and did not get to finish making the footer functional.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "SkillsUSARealImages")));
 
 app.get("/", (req, res) => {
 	res.render("index");
@@ -15,6 +16,38 @@ app.get("/", (req, res) => {
 
 app.get("/bars", (req, res) => {
     res.render("bars");
+});
+
+app.get("/about", (req, res) => {
+	res.render("about");
+});
+
+app.get("/staff", (req, res) => {
+	res.render("staff");
+});
+
+app.get("/competitions", (req, res) => {
+	res.render("competitions");
+});
+
+app.get("/contact", (req, res) => {
+	res.render("contact");
+});
+
+app.get("/donate", (req, res) => {
+	res.render("donate");
+});
+
+app.get("/sponsor", (req, res) => {
+	res.render("sponsor");
+});
+
+app.get("/news", (req, res) => {
+	res.render("news");
+});
+
+app.get("/photos", (req, res) => {
+	res.render("photos");
 });
 
 app.listen(PORT, () => {
